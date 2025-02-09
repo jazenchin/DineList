feaf94dce39029a64c0b7267442bd49e0ea8b9ee
cl940222 <cl940222@gmail.com>
2025-02-09 08:11:49 +0800

    feat: 編輯器新增日期/早午餐/符號選項

--- popup.js
+++ popup.js
@@ -1,23 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const editTemplateBtn = document.getElementById('editTemplate');
   const togglePanelBtn = document.getElementById('togglePanel');
-  const showOutputBtn = document.getElementById('showOutput');
-
-  editTemplateBtn.addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: 'editTemplate'});
-    });
-  });
-
   togglePanelBtn.addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {action: 'togglePanel'});
-    });
-  });
-
-  showOutputBtn.addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: 'showOutput'});
     });
   });
 }); 
